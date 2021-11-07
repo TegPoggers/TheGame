53c1e450b3b50b5b22912cc44742a024b3fbb2f1
Viviane Ruotolo <vivianeruotolo@hotmail.com>
2021-11-07 15:59:46 -0300

    Resolvendo conflitos

---
+++ main.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+#include "Game.h"
+
+int main()
+{
+
+    saveSaps::Game game;
+
+    game.run();
+
+    std::cout << "Plim plim plim o meu jogo faz assim" << std::endl;
+
+    return 0;
+}
+
+
