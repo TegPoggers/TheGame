51e3f8902377ec232c27836eefd66805c47ffc95
David Segalle <davidsegalle@alunos.utfpr.edu.br>
2021-11-23 19:55:18 -0300

    Criei 1 classe

---
+++ StaticEntity.cpp
@@ -0,0 +1,5 @@
+//
+// Created by segalle on 11/23/21.
+//
+
+#include "StaticEntity.h"
---
+++ StaticEntity.h
@@ -0,0 +1,18 @@
+//
+// Created by segalle on 11/23/21.
+//
+
+#ifndef JOGO_STATICENTITY_H
+#define JOGO_STATICENTITY_H
+
+#include "Entity.h"
+
+namespace entities {
+
+    class StaticEntity {
+
+    };
+
+}
+
+#endif //JOGO_STATICENTITY_H
