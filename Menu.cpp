29fdb78e483ca2d9b19132eaf74b1043eb8e089d
Viviane Ruotolo <vivianeruotolo@hotmail.com>
2021-11-25 18:23:10 -0300

    Início do Menu + Star com escala

---
+++ Menu.cpp
@@ -0,0 +1,5 @@
+//
+// Created by viviane on 25/11/2021.
+//
+
+#include "Menu.h"
---
+++ Menu.h
@@ -0,0 +1,21 @@
+//
+// Created by viviane on 25/11/2021.
+//
+
+#ifndef JOGO_MENU_H
+#define JOGO_MENU_H
+
+#include "Being.h"
+
+
+class Menu : Being{
+private:
+    int selectedItem;
+    //sf::font
+
+public:
+
+};
+
+
+#endif //JOGO_MENU_H
